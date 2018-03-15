1aa2dcc55bcdfbcfd3a079ebf6ef6bffb81a41da
alu20925473g <alu20925473g@ti2-11>
2018-03-15 20:22:12 +0100

    solved consecutive calls to oprators (and equal)

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -369,35 +369,47 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
         // TODO add your handling code here:
+        if(!erase){
         erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.ADD;
+      }
 
     }//GEN-LAST:event_btnAddActionPerformed
 
     private void btnSubtractActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubtractActionPerformed
-       erase = true;
+       if(!erase){
+        erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.SUBTRACT;
+      }
     }//GEN-LAST:event_btnSubtractActionPerformed
 
     private void btnMultiplyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMultiplyActionPerformed
-       erase = true;
+      if(!erase){
+        erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.MULTIPLY;
+      }
     }//GEN-LAST:event_btnMultiplyActionPerformed
 
     private void btnDivideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDivideActionPerformed
-erase = true;
+if(!erase){
+        erase = true;
         calculateResult();
         displayResult();
-        operator = OperatorType.DIVIDE;    }//GEN-LAST:event_btnDivideActionPerformed
+        operator = OperatorType.DIVIDE;
+      }    }//GEN-LAST:event_btnDivideActionPerformed
 
     private void btnEqualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEqualActionPerformed
-        // TODO add your handling code here:
+        if(!erase){
+        erase = true;
+        calculateResult();
+        displayResult();
+        }
     }//GEN-LAST:event_btnEqualActionPerformed
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
